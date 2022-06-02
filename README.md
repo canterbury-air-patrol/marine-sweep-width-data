e268fdd1dc9dcdb48af00ca561f0b7beb756fed2
Scott Parlane <scott@parlanenz.com>
2022-06-02 21:28:53 +1200

    Create initial source data
    Data taken from tables H-10 to H-19 of
    "U. S. COAST GUARD ADDENDUM TO THE UNITED STATES NATIONAL SEARCH AND RESCUE SUPPLEMENT (NSS) To The International Aeronautical and Maritime Search and Rescue Manual (IAMSAR)", January 2013
    https://www.dco.uscg.mil/Portals/9/CG-5R/manuals/COMDTINST%20M16130.2F.pdf
    
    The script import_data.py processes the csv files into a js file that exports the data as a single table

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# marine-search-area-coverage-data
+# Search Area Coverage Data
+
+Data here is extracted from [https://www.dco.uscg.mil/Portals/9/CG-5R/manuals/COMDTINST%20M16130.2F.pdf]
+
---
+++ import_data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+import csv
+import json
+
+corrections_file = 'weather_correction_types.csv'
+directory = 'source-data'
+
+data = {}
+
+def merge_sw_distance(sweep_widths, distances):
+    result = []
+    last_sw = -1
+    for sw, viz in zip(sweep_widths, distances):
+        if sw == last_sw:
+            continue
+        result.append({'sw': sw, 'vis': viz})
+        last_sw = sw
+    return result
+
+def load_corrections(corrections_file):
+    with open(corrections_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            if line[0] not in data:
+                data[line[0]] = {}
+            data[line[0]]['weather_corrections'] = line[1]
+
+
+def load_sweep_widths(directory):
+    for f in os.listdir(directory):
+        short_name = f.split('.')[0]
+        asset_type, height = short_name.split('-')
+
+        filename = os.path.join(directory, f)
+        if os.path.isfile(filename):
+            distances = []
+            with open(filename, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for line in reader:
+                    if line[0] == 'Search Object':
+                        distances = line[1:]
+                        continue
+                    if line[0] not in data:
+                        data[line[0]] = {}
+                    if asset_type not in data[line[0]]:
+                        data[line[0]][asset_type] = {}
+                    data[line[0]][asset_type][height] = merge_sw_distance(line[1:],distances)
+
+load_corrections(corrections_file)
+load_sweep_widths(directory)
+
+with open('marine-sweep-width-data.js', 'w') as output:
+    output.write('const marine_sweep_widths =\n')
+    output.write(json.JSONEncoder().encode(data))
+    output.write(';\n')
+    output.write("export {'marine_sweep_widths'};")
