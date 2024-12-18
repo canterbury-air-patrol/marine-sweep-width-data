ebcd55457487d29853a0fbe606450b4f5601920c
Scott Parlane <scott@parlanenz.com>
2024-12-18 21:41:22 +1300

    Convert to using typescript
    Exposes the types as well so they can be used when the data is passed around

---
+++ marine-sweep-width-data.ts
@@ -0,0 +1,5 @@
+import { marine_sweep_widths } from './marine-sweep-width-data-table';
+import { marine_sweep_width_weather_corrections } from './marine-sweep-width-weather-corrects';
+import { MarineSweepWidthData, SearchTargetSweepWidthData, SearchHeightSweepWidths, SearchSpeedCorrections, SearchSweepWidthVisibilityData, SearchSpeedCorrection } from './marine-sweep-width-types';
+
+export { marine_sweep_widths, marine_sweep_width_weather_corrections, MarineSweepWidthData, SearchTargetSweepWidthData, SearchHeightSweepWidths, SearchSpeedCorrections, SearchSweepWidthVisibilityData, SearchSpeedCorrection };
