04c09069f64072f408a3b4bc3f91918437119bd6
Scott Parlane <scott@parlanenz.com>
2024-12-18 23:07:43 +1300

    Update version and fix entry file name
    Also setup the build command

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['marine-leeway-table.ts'],
+  entry: ['marine-sweep-width-data.ts'],
   format: ['cjs', 'esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
